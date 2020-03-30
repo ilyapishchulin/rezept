7096a0758320ad3e9953f2299d7c5be9b2daf661
ilyapishchulin <ilyapishchulin@mail.ru>
2020-03-30 18:33:53 +0300

    Covid-19

---
+++ precache-manifest.acc378900360d3c0617e6d64531456fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9fbd83765a6604067e32a9804a7607",
+    "url": "/rezept/index.html"
+  },
+  {
+    "revision": "1df505a4aaa1f81fb551",
+    "url": "/rezept/static/css/2.867227ff.chunk.css"
+  },
+  {
+    "revision": "1df505a4aaa1f81fb551",
+    "url": "/rezept/static/js/2.ed479890.chunk.js"
+  },
+  {
+    "revision": "4c5e451650fc2af6d282",
+    "url": "/rezept/static/js/main.80bc8b00.chunk.js"
+  },
+  {
+    "revision": "afc435e7432eba8f1a9a",
+    "url": "/rezept/static/js/runtime-main.0c857277.js"
+  }
+]);
